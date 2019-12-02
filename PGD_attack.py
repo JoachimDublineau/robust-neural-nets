4be1504b4925391612a0c71a311118baf4fc671f
JoachimDublineau <48889312+JoachimDublineau@users.noreply.github.com>
2019-12-02 14:38:33 +0100

    Update PGD_attack.py

--- PGD_attack.py
+++ PGD_attack.py
@@ -25,21 +25,34 @@ def generate_perturbation(img_size, eps, norm = "l2"):
         perturbation *= np.random.random(eps)/pert_norm
     return perturbation
 
-# Test generate_perturbation
-perturbation = generate_perturbation((10,10), 1, norm = "l2")
-print(perturbation)
-print(np.linalg.norm(perturbation))
+# # Test generate_perturbation
+# perturbation = generate_perturbation((10,10), 1, norm = "l2")
+# print(perturbation)
+# print(np.linalg.norm(perturbation))
+
+def compute_grad(func, point, delta):
+    func = np.reshape(func, -1)
+    for 
 
 def maximize_loss(model, loss, ref_image, rate, eps, norm = "l2", 
                   nb_it = 10):
     size = ref_image.shape
-    prev_perturbation = ref_image
-    curr_perturbation = 0
+    prev_perturbation  = np.zeros(size, dtype = int)
+    prev_perturbated_img = ref_image
+    prev_prediction = model.predict([prev_perturbated_img])
+    prev_loss = 0
+    print(prev_prediction.shape)
+    curr_perturbation = generate_perturbation(size, eps, norm)
     for iter in range(nb_it):
-        curr_pertubation = generate_perturbation(size, eps, norm)
         curr_perturbated_img = ref_image + curr_perturbation
-        prev_perturbated_img = ref_image + prev_perturbation
-        curr_loss = K.eval(loss()
+        # prev_perturbated_img = ref_image + prev_perturbation
+        curr_prediction = model.predict([curr_perturbated_img])
+        curr_loss = K.eval(loss(curr_prediction, prev_prediction))
+        grad = (curr_loss - prev_loss)/np.linalg.norm(curr_pertubation /
+            - prev_perturbation)
+        prev_perturbation = curr_perturbation
+        curr_perturbation += rate *  
+        
         # compute the loss of previous pert and current pert
         # deduces the gradient of the loss
         # compute the new pert and updates the previous pert
