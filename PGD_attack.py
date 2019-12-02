9c80823c58874e0a5b62f2ac0098b3e08c3b4692
Joachim_Dublineau <Joachim_Dublineau@users.noreply.github.com>
2019-12-02 17:02:56 +0100

    Update PGD_attack.py

--- PGD_attack.py
+++ PGD_attack.py
@@ -1,6 +1,7 @@
 import numpy as np
 from keras import backend as K 
 from keras.losses import categorical_crossentropy
+import tensorflow as tf
 
 def generate_perturbation(img_size, eps, norm = "l2"):
     """
@@ -30,30 +31,47 @@ def generate_perturbation(img_size, eps, norm = "l2"):
 # print(perturbation)
 # print(np.linalg.norm(perturbation))
 
-def compute_grad(func, point, delta):
-    func = np.reshape(func, -1)
-    for 
+def compute_grad(model, loss, image, input_label):
+    """
+    Inspired from tensorflow core. 
+    INPUTS:
+    - model: tensorflow model
+    - loss: tensorflow loss
+    - image: reference image
+    - input_label: one-hot encoding of the class
+    - norm: 'l2' or 'inf' depending on what norm do you want.
+    OUTPUTS:
+    - perturbation: numpy array of dimension img_size.
+    COMPUTATION TIME:
+    In n*p
+    """
+    with tf.GradientTape() as tape:
+        tape.watch(input_image)
+        prediction = model(input_image)
+        loss_ = loss(input_label, prediction)
+    gradient = tape.gradient(loss_, input_image)
+    signed_grad = tf.sign(gradient)
+    return signed_grad
 
-def maximize_loss(model, loss, ref_image, rate, eps, norm = "l2", 
-                  nb_it = 10):
-    size = ref_image.shape
-    prev_perturbation  = np.zeros(size, dtype = int)
-    prev_perturbated_img = ref_image
-    prev_prediction = model.predict([prev_perturbated_img])
-    prev_loss = 0
-    print(prev_prediction.shape)
-    curr_perturbation = generate_perturbation(size, eps, norm)
+def projection(point, ref, eps, norm):
+    dist = np.linalg.norm(ref - point, ord = norm)
+    if dist < eps:
+        return point
+    return ref + eps*(point - ref)/dist 
+
+# # Test projection:
+# a = np.array([1,1], dtype = np.float32)
+# b = np.array([0,0], dtype = np.float32)
+# print(a-b)
+# print(np.linalg.norm(a-b, ord=2))
+# print(projection(a, b, 1, 2))
+
+def maximize_loss(model, loss, ref_image, y_image, rate, eps, 
+                  norm = 2, nb_it = 10):
+    curr_perturbation = rate*compute_grad(model, loss, ref_image, y_image)
     for iter in range(nb_it):
         curr_perturbated_img = ref_image + curr_perturbation
-        # prev_perturbated_img = ref_image + prev_perturbation
-        curr_prediction = model.predict([curr_perturbated_img])
-        curr_loss = K.eval(loss(curr_prediction, prev_prediction))
-        grad = (curr_loss - prev_loss)/np.linalg.norm(curr_pertubation /
-            - prev_perturbation)
-        prev_perturbation = curr_perturbation
-        curr_perturbation += rate *  
-        
-        # compute the loss of previous pert and current pert
-        # deduces the gradient of the loss
-        # compute the new pert and updates the previous pert
-    return curr_pertubation
+        signed_grad = compute_grad(model, loss, curr_perturbated_img, y_image)
+        curr_perturbation += projection(rate*signed_grad,ref_image,eps,norm)
+    return curr_perturbation
+
