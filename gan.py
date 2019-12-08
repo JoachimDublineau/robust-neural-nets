8711e7d69d961096dfcceacb1235df5f278c5bc8
Elie KADOCHE <eliekadoche78@gmail.com>
2019-12-08 18:39:36 +0100

    Added losses and optimizers

--- gan.py
+++ gan.py
@@ -168,5 +168,27 @@ def build_discriminator_model():
     return discriminator_model
 
 
-# Train model
+# Losses and optimizers
 # -------------------------
+
+# This method returns a helper function to compute cross entropy loss
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+
+def generator_loss(fake_output):
+    """ Compare the discriminator decisions on the generated image to an array of 1s
+    """
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+
+def discriminator_loss(real_output, fake_output):
+    """ Compares the discriminator's predictions on real image to an array os 1s (0s for generated images)
+    """
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
