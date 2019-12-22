d11562b5c4cc65eefbc823d15f6d648b6480aefd
JoachimDublineau <48889312+JoachimDublineau@users.noreply.github.com>
2019-12-22 19:29:08 +0100

    Update test_one_pixel_attack.py
    Update

--- test_one_pixel_attack.py
+++ test_one_pixel_attack.py
@@ -18,19 +18,38 @@ y_test = tf.keras.utils.to_categorical(y_test, \
     num_classes = len(src.cifar10.labels))
 
 model = tf.keras.models.load_model("models/cifar10_simple_model_73_acc.h5")
-batch_size = 10
-random_indexes = np.random.choice(x_test.shape[0], 100)
-images = x_train[random_indexes]
-labels = y_train[random_indexes]
-attacks = generate_one_pixel_attacks(model, images, labels)
+batch_size = 50
+random_indexes = np.random.choice(x_test.shape[0], 500)
+images = x_test[random_indexes]
+labels = y_test[random_indexes]
+attacks = generate_one_pixel_attacks(model, images, labels, batch_size)
+
 fig=plt.figure(figsize=(1, 2))
 fig.add_subplot(1, 2, 1)
 plt.imshow(images[0])
 fig.add_subplot(1, 2, 2)
 plt.imshow(attacks[0])
 plt.show()
-predictions_on_pert = np.argmax(model(K.cast(attacks, dtype = 'float32')), axis=1)
-predictions = np.argmax(model(K.cast(images,dtype = 'float32')), axis=1)
-nb_mistakes = np.count_nonzero(predictions_on_pert-predictions)
-damages = nb_mistakes/images.shape[0]
-print(damages)
+
+fig=plt.figure(figsize=(1, 2))
+fig.add_subplot(1, 2, 1)
+plt.imshow(images[1])
+fig.add_subplot(1, 2, 2)
+plt.imshow(attacks[1])
+plt.show()
+
+fig=plt.figure(figsize=(1, 2))
+fig.add_subplot(1, 2, 1)
+plt.imshow(images[2])
+fig.add_subplot(1, 2, 2)
+plt.imshow(attacks[2])
+plt.show()
+
+def compute_accuracy(model, x, y, batch_size):
+    attacks = generate_one_pixel_attacks(model, x, y, batch_size)
+    scores = model.evaluate(attacks, y)
+    return scores[1]
+
+print(model.evaluate(images, labels)[1])
+print(compute_accuracy(model, images, labels, batch_size))
+
